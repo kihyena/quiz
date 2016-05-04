812f732365b000e56d43578f99a94118cb9fabbf
kihyena <ps10david@hotmail.com>
2016-05-04 22:41:13 +0200

    Mensajes flash

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-sesion');
 var partials =require('express-partials'); 
+var flash = require('express-flash');
 
 var routes = require('./routes/index');
 
@@ -15,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(partials());
+app.use(flash());
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,6 +25,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({secret: "Quiz 2016", resave: false, seveUninitialized: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
