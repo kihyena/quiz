53a5bba31418e303778dbf924dd3ee3c97ce8648
kihyena <ps10david@hotmail.com>
2016-05-05 00:00:00 +0200

    Edición pregunta

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,9 @@ router.get('/quizzes/:quizId(\\d+)', quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 router.get('/quizzes/new', quizController.new);
 router.post('/quizzes', quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizzes/:quizId(\\d+)', quizController.update);
+router.delete('/quizzes/:quizId(\\d+)', quizController.destroy);
 //router.get('/check', quizController.check);
 router.get('/author', function(req, res, next){
   res.render('author');
