9b4144cf2dad876b8725fcaae83b2ed3a2f61794
kihyena <ps10david@hotmail.com>
2016-05-31 21:03:02 +0200

    Usuarios2

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
