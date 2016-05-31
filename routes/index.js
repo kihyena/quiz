f5221ecc8fcbb84bc630ca14ad282d589240ce2c
kihyena <ps10david@hotmail.com>
2016-05-31 20:52:20 +0200

    Usuarios

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,19 @@ var commentController = require('../controllers/comment_controller');
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-//Autoload de rutas que usen :quizId
+//Autoload de parametros
 router.param('quizId', quizController.load); //Autoload :quizId
+router.param('userId', userController.load); //autoload :userId
+
+//Definición de rutas de cuenta
+router.get('/users', userController.index);
+router.get('/users/:userId(\\d+)', userController.show);
+router.get('/users/new', userController.new);
+router.post('/users', userController.create);
+router.get('/users/:userId(\\d+)/edit', userController.edit);
+router.put('/users/:userId(\\d+)', userController.update);
+router.delete('/users/:userId(\\d+)', userController.destroy);
+
 //router.get('/question', quizController.question);
 router.get('/quizzes.:format(\\w+)?', quizController.index);
 router.get('/quizzes/:quizId(\\d+).:format(\\w+)?', quizController.show);
